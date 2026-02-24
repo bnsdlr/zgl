f4e3eaecab83d4c008942e011fff2b282cc22725
bnsdlr <benhamster443@gmail.com>
2026-02-24 20:19:19 +0100

    triangl moving horizontaly

---
+++ src/helper/shader.zig
@@ -0,0 +1,80 @@
+const std = @import("std");
+const c = @import("../c.zig");
+const gl = c.glad;
+
+id: u32 = 0,
+
+const Self = @This();
+
+pub fn construct(vertex_shader_code: [*:0]const u8, fragment_shader_code: [*:0]const u8) !Self {
+    var success: c_int = 0;
+    var info_log_length: u32 = 0;
+    var info_log: [512]u8 = undefined;
+
+    const vertex_shader: u32 =
+        loadShader(vertex_shader_code, gl.GL_VERTEX_SHADER, &success, &info_log_length);
+    defer gl.glDeleteShader(vertex_shader);
+
+    if (success == 0) {
+        gl.glGetShaderInfoLog(vertex_shader, info_log.len, null, &info_log);
+        std.log.err("Failed to compile vertex shader:\n{s}", .{info_log[0..info_log_length]});
+        return error.ShaderCompilationFailed;
+    }
+
+    const fragment_shader: u32 =
+        loadShader(fragment_shader_code, gl.GL_FRAGMENT_SHADER, &success, &info_log_length);
+    defer gl.glDeleteShader(fragment_shader);
+
+    if (success == 0) {
+        gl.glGetShaderInfoLog(fragment_shader, info_log.len, null, &info_log);
+        std.log.err("Failed to compile fragment shader:\n{s}", .{info_log[0..info_log_length]});
+        return error.ShaderCompilationFailed;
+    }
+
+    const program: u32 = gl.glCreateProgram();
+    gl.glAttachShader(program, vertex_shader);
+    gl.glAttachShader(program, fragment_shader);
+    gl.glLinkProgram(program);
+    gl.glGetProgramiv(program, gl.GL_LINK_STATUS, &success);
+    gl.glGetProgramiv(program, gl.GL_INFO_LOG_LENGTH, @ptrCast(&info_log_length));
+
+    if (success == 0) {
+        gl.glGetProgramInfoLog(program, info_log.len, null, &info_log);
+        std.log.err("Failed to link shader program:\n{s}", .{info_log[0..info_log_length]});
+        return error.ShaderLinkingFailed;
+    }
+
+    return Self{ .id = program };
+}
+
+fn loadShader(code: [*:0]const u8, kind: gl.GLenum, success: *c_int, info_log_length: *u32) u32 {
+    // var info_log_len: c_int = 0;
+    const shader: u32 = gl.glCreateShader(kind);
+    gl.glShaderSource(shader, 1, &code, null);
+    gl.glCompileShader(shader);
+    gl.glGetShaderiv(shader, gl.GL_COMPILE_STATUS, success);
+    gl.glGetShaderiv(shader, gl.GL_INFO_LOG_LENGTH, @ptrCast(info_log_length));
+
+    // if (info_log_len > 0) {
+    //     info_log_length.* = info_log_len;
+    // }
+
+    return shader;
+}
+
+/// use/activate the shader
+pub fn use(self: *const Self) void {
+    gl.glUseProgram(self.id);
+}
+
+pub fn setBool(self: *const Self, name: [*:0]const u8, value: bool) void {
+    gl.glUniform1i(gl.glGetUniformLocation(self.id, name), @bitCast(value));
+}
+
+pub fn setInt(self: *const Self, name: [*:0]const u8, value: c_int) void {
+    gl.glUniform1i(gl.glGetUniformLocation(self.id, name), value);
+}
+
+pub fn setFloat(self: *const Self, name: [*:0]const u8, value: f32) void {
+    gl.glUniform1f(gl.glGetUniformLocation(self.id, name), value);
+}
--- src/root.zig
+++ src/root.zig
@@ -4,6 +4,8 @@ const c = @import("c.zig");
 const glfw = c.glfw;
 const gl = c.glad;
 
+const Shader = @import("helper/shader.zig");
+
 const Window = c.Window;
 
 pub const glfw_error = error{
@@ -59,87 +61,15 @@ pub fn run() (glad_error || glfw_error)!void {
     // gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE);
 
     // build and compile shader program
-    var success: c_int = 0;
-    var info_log_length: u32 = 0;
-    var info_log: [512]u8 = undefined;
-
-    // vertex shader
-    const vs_src: [*c]const u8 = @embedFile("assets/shaders/vertex.glsl");
-    const vertex_shader: u32 =
-        getCompiledShader(vs_src, &success, &info_log_length, gl.GL_VERTEX_SHADER);
-    defer gl.glDeleteShader(vertex_shader);
-
-    if (success == 0) {
-        gl.glGetShaderInfoLog(vertex_shader, info_log.len, null, &info_log);
-        std.log.err("Failed to compile vertex shader:\n{s}", .{info_log[0..info_log_length]});
-        return glad_error.ShaderCompilationFailed;
-    }
-
-    // fragment shader
-    const fs_src: [*c]const u8 = @embedFile("assets/shaders/fragment.glsl");
-    const fragment_shader: u32 =
-        getCompiledShader(fs_src, &success, &info_log_length, gl.GL_FRAGMENT_SHADER);
-    defer gl.glDeleteShader(fragment_shader);
-
-    if (success == 0) {
-        gl.glGetShaderInfoLog(fragment_shader, info_log.len, null, &info_log);
-        std.log.err("Failed to compile fragment shader:\n{s}", .{info_log[0..info_log_length]});
-        return glad_error.ShaderCompilationFailed;
-    }
-
-    // shader program
-    const shader_program: u32 = gl.glCreateProgram();
-    defer gl.glDeleteProgram(shader_program);
-
-    gl.glAttachShader(shader_program, vertex_shader);
-    gl.glAttachShader(shader_program, fragment_shader);
-    gl.glLinkProgram(shader_program);
-
-    gl.glGetProgramiv(shader_program, gl.GL_LINK_STATUS, &success);
-    // gl.glGetProgramiv(shader_program, gl.GL_INFO_LOG_LENGTH, &info_log_length);
-
-    if (success == 0) {
-        gl.glGetProgramInfoLog(shader_program, info_log.len, null, &info_log);
-        std.log.err("Failed to link shaders:\n{s}", .{info_log[0..info_log_length]});
-        return glad_error.ShaderLinkingFailed;
-    }
-
-    const fs2_src = @embedFile("assets/shaders/fragment2.glsl");
-    const fs2 = getCompiledShader(fs2_src, &success, &info_log_length, gl.GL_FRAGMENT_SHADER);
-    defer gl.glDeleteShader(fs2);
-
-    if (success == 0) {
-        gl.glGetShaderInfoLog(fs2, info_log.len, null, &info_log);
-        std.log.err("Failed to compile fragment shader 2:\n{s}", .{info_log[0..info_log_length]});
-        return glad_error.ShaderCompilationFailed;
-    }
-
-    const shader_program2: u32 = gl.glCreateProgram();
-    defer gl.glDeleteProgram(shader_program2);
-
-    gl.glAttachShader(shader_program2, vertex_shader);
-    gl.glAttachShader(shader_program2, fs2);
-    gl.glLinkProgram(shader_program2);
-
-    gl.glGetProgramiv(shader_program2, gl.GL_LINK_STATUS, &success);
-
-    if (success == 0) {
-        gl.glGetProgramInfoLog(shader_program, info_log.len, null, &info_log);
-        std.log.err("Failed to link shaders:\n{s}", .{info_log[0..info_log_length]});
-        return glad_error.ShaderLinkingFailed;
-    }
+    const shader: Shader =
+        try .construct(@embedFile("assets/shaders/vertex.glsl"), @embedFile("assets/shaders/fragment.glsl"));
+    defer gl.glDeleteProgram(shader.id);
 
     // vertex data and configure vertex attributes
-    const vertices1 = [_][3]f32{
-        .{ 0.5, 0.5, 0.0 },
-        .{ -0.5, 0.5, 0.0 },
-        .{ -1.0, -0.5, 0.0 },
-    };
-    const vertices2 = [_][3]f32{
-        .{ 0.5, 0.5, 0.0 },
-        .{ 0.5, -0.5, 0.0 },
-        // .{ -0.5, -0.5, 0.0 },
-        .{ -0.5, 0.5, 0.0 },
+    const vertices = [_][6]f32{
+        .{ 0.5, -0.5, 0.0, 1.0, 0.0, 0.0 },
+        .{ -0.5, -0.5, 0.0, 0.0, 1.0, 0.0 },
+        .{ 0.0, 0.5, 0.0, 0.0, 0.0, 1.0 },
     };
 
     // const indices = [_][3]u32{
@@ -148,39 +78,34 @@ pub fn run() (glad_error || glfw_error)!void {
     // };
 
     // buff 1
-    var vao1: u32 = 0;
-    defer gl.glDeleteVertexArrays(1, &vao1);
-    gl.glGenVertexArrays(1, &vao1);
-    gl.glBindVertexArray(vao1);
+    var vao: u32, var vbo: u32 = .{ 0, 0 };
+    defer gl.glDeleteVertexArrays(1, &vao);
+    defer gl.glDeleteBuffers(1, &vbo);
 
-    var vbo1: u32 = 0;
-    defer gl.glDeleteBuffers(1, &vbo1);
-    gl.glGenBuffers(1, &vbo1);
-    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo1);
-    gl.glBufferData(gl.GL_ARRAY_BUFFER, @sizeOf(@TypeOf(vertices1)), &vertices1, gl.GL_STATIC_DRAW);
+    gl.glGenVertexArrays(1, &vao);
+    gl.glGenBuffers(1, &vbo);
 
-    gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 3 * @sizeOf(f32), @ptrFromInt(0));
+    gl.glBindVertexArray(vao);
+
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo);
+    gl.glBufferData(gl.GL_ARRAY_BUFFER, @sizeOf(@TypeOf(vertices)), &vertices, gl.GL_STATIC_DRAW);
+
+    // position
+    gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 6 * @sizeOf(f32), @ptrFromInt(0));
     gl.glEnableVertexAttribArray(0);
+    // color
+    gl.glVertexAttribPointer(1, 3, gl.GL_FLOAT, gl.GL_FALSE, 6 * @sizeOf(f32), @ptrFromInt(3 * @sizeOf(f32)));
+    gl.glEnableVertexAttribArray(1);
+
+    gl.glBindVertexArray(0);
+
+    shader.use();
 
     // var ebo: u32 = 0;
     // defer gl.glDeleteBuffers(1, &ebo);
     // gl.glGenBuffers(1, &ebo);
     // gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, ebo);
     // gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER, @sizeOf(@TypeOf(indices)), &indices, gl.GL_STATIC_DRAW);
-
-    var vao2: u32 = 0;
-    defer gl.glDeleteVertexArrays(1, &vao2);
-    gl.glGenVertexArrays(1, &vao2);
-    gl.glBindVertexArray(vao2);
-
-    var vbo2: u32 = 0;
-    defer gl.glDeleteBuffers(1, vbo2);
-    gl.glGenBuffers(1, &vbo2);
-    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo2);
-    gl.glBufferData(gl.GL_ARRAY_BUFFER, @sizeOf(@TypeOf(vertices2)), &vertices2, gl.GL_STATIC_DRAW);
-
-    gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 3 * @sizeOf(f32), @ptrFromInt(0));
-    gl.glEnableVertexAttribArray(0);
 
     // main loop
     while (glfw.glfwWindowShouldClose(window) == 0) {
@@ -189,16 +114,13 @@ pub fn run() (glad_error || glfw_error)!void {
         gl.glClearColor(0.2, 0.5, 0.5, 1);
         gl.glClear(gl.GL_COLOR_BUFFER_BIT);
 
-        gl.glUseProgram(shader_program);
-        gl.glBindVertexArray(vao1);
-        gl.glDrawArrays(gl.GL_TRIANGLES, 0, 3);
-        // gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, ebo);
-        // gl.glDrawElements(gl.GL_TRIANGLES, indices.len * indices[0].len, gl.GL_UNSIGNED_INT, @ptrFromInt(0));
+        const time_value = glfw.glfwGetTime();
+        const horizontal_offset: f32 = @floatCast(std.math.sin(time_value));
 
-        gl.glUseProgram(shader_program2);
-        gl.glBindVertexArray(vao2);
-        gl.glDrawArrays(gl.GL_TRIANGLES, 0, 3);
+        shader.setFloat("horizontalOffset", horizontal_offset);
 
+        gl.glBindVertexArray(vao);
+        gl.glDrawArrays(gl.GL_TRIANGLES, 0, 3);
         gl.glBindVertexArray(0);
 
         glfw.glfwSwapBuffers(window);
@@ -215,17 +137,4 @@ fn processInput(window: Window) void {
 fn framebufferSizeCallback(_: Window, width: c_int, height: c_int) callconv(.c) void {
     // std.debug.print("Window size updated: (width: {d}, height: {d})\n", .{ width, height });
     gl.glViewport(0, 0, width, height);
-}
-
-fn getCompiledShader(src: [*c]const u8, success: *c_int, info_log_length: *u32, kind: gl.GLenum) u32 {
-    var log_len: c_int = 0;
-    const shader: u32 = gl.glCreateShader(kind);
-    gl.glShaderSource(shader, 1, &src, null);
-    gl.glCompileShader(shader);
-    gl.glGetShaderiv(shader, gl.GL_COMPILE_STATUS, success);
-    gl.glGetShaderiv(shader, gl.GL_INFO_LOG_LENGTH, &log_len);
-
-    info_log_length.* = if (log_len >= 0) @intCast(log_len) else 0;
-
-    return shader;
 }
